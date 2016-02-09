2de64aeacf11d34b5b3ef759a0086089524d63a5
Morten Hulvej Andersen <hulvei3@gmail.com>
2016-02-09 15:11:06 +0100

    added bootstrap-components

--- page.jsx
+++ page.jsx
@@ -1,10 +1,27 @@
 var React = require('react');
 
+var { Button, ButtonToolbar, ListGroup, ListGroupItem } = require('react-bootstrap');
+
+var HeadTag = React.createClass({
+
+	render: function() {
+		return (
+			<head>
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
+				<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossOrigin="anonymous"></script>
+			</head>
+		);
+	}
+});
+
 var Header = React.createClass({
 	render: function() {
-		return <nav style={{padding: '20px'}}>
-			<li><a href="/">HOME</a></li>
-		</nav>
+		return <ButtonToolbar>
+			<Button href="/">Home</Button>
+			<Button href="/public" bsStyle="info">Assignments</Button>
+			<Button href="/course-info/details.md" bsStyle="info">Course-info</Button>
+		</ButtonToolbar>
 	}
 });
 
@@ -13,9 +30,9 @@ var List = React.createClass({
 		items: React.PropTypes.array
 	},
 	render: function() {
-		return <div>
-			<ul>{this.props.items.map(item => <li key={item.path}><a href={item.path}>{item.name}</a></li>)}</ul>
-		</div>
+		return <ListGroup>
+			{this.props.items.map(item => <ListGroupItem href={item.path}>{item.name}</ListGroupItem>)}
+		</ListGroup>
 	}
 });
 
@@ -23,14 +40,16 @@ module.exports = React.createClass({
 	displayName: 'Page',
 	render: function() {
 		return <html>
-			<head>
-				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossOrigin="anonymous"></script>
-			</head><body>
-			<div>
-				<Header />
-				<List {...this.props} />
-			</div></body></html>
+			<HeadTag />
+			<body style={{padding: '30px'}}>
+				<div>
+					<Header />
+					<div style={{padding: '30px 0'}}>
+						<List {...this.props} />
+					</div>
+				</div>
+			</body>
+		</html>
 	}
 
 });
