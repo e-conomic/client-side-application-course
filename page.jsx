c661e96e5a0b103303111458bdcd029c633b86c7
Morten Hulvej Andersen <hulvei3@gmail.com>
2016-02-09 12:52:26 +0100

    added css

--- page.jsx
+++ page.jsx
@@ -22,10 +22,15 @@ var List = React.createClass({
 module.exports = React.createClass({
 	displayName: 'Page',
 	render: function() {
-		return <div>
-			<Header />
-			<List {...this.props} />
-		</div>
+		return <html>
+			<head>
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossOrigin="anonymous"></script>
+			</head><body>
+			<div>
+				<Header />
+				<List {...this.props} />
+			</div></body></html>
 	}
 
 });
