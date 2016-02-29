9b199025990569d136ed887ecac2ff979f514134
Calin <calinilielaur@gmail.com>
2016-02-29 11:23:30 +0100

    fixed indentation errors

--- denlillemand/week3/ass4/app/app.jsx
+++ denlillemand/week3/ass4/app/app.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainList from './mainlist.jsx';
 
-ReactDOM.render(<MainList />, document.getElementById("root"));
+ReactDOM.render(<MainList />, document.getElementById('root'));
