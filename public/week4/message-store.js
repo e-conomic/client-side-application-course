ee54488d6266f2c8c447c8f642ffa3197ff438b1
Morten Hulvej Andersen <hulvei3@gmail.com>
2016-03-01 15:14:38 +0100

    added skeleton for week4

---
+++ public/week4/message-store.js
@@ -0,0 +1,21 @@
+var Dispatcher = require('flux').Dispatcher;
+var Constants = require('./constants');
+var BaseStore = require('./base');
+
+store = Object.assign({}, BaseStore, {
+	// TODO
+});
+
+store.dispatchToken = Dispatcher.register(function(payload){
+
+	switch(payload.type) {
+
+		default:
+			return;
+	}
+
+	store.emitChange();
+});
+
+return store;
+
