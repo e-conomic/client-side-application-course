abc21aa8c0364f5d3bd8328c1687b893d8f48fcf
pcolliander <pontus.colliander@gmail.com>
2016-03-01 08:41:15 +0100

    finished assignment 4

---
+++ pontus/week3/app/app.jsx
@@ -0,0 +1,84 @@
+let React = require('react');
+let NamedList = require('./named-list');
+let ReactDOM = require('react-dom');
+
+let Wrapper = React.createClass({ 
+	getInitialState() { 
+		return { 
+			lists: [],
+			messages: []
+		};
+	},
+
+	createList(e) { 
+		let listID = Date.now();
+		let list;
+		if (e.keyCode == 13 || e.which == 13 || e.type == 'click') { 
+			if (this.refs.inputField.value != '') list = { listID, listName: this.refs.inputField.value }
+			else list = { listID, listName: "" };
+
+			this.setState({ lists: this.state.lists.concat(list) });
+			this.refs.inputField.value = "";
+		}
+	},
+
+	createMsg(listID, text) {
+		let message = {messageID: Date.now(), listID, text, isArchived: false};
+		this.setState({ messages: this.state.messages.concat(message) });
+	},
+
+	moveMsg(listID, messageID) { 
+		let message = this.state.messages.filter( message => message.messageID == messageID )[0];
+		let restOfmessages = this.state.messages.filter( message => message.messageID != messageID );
+		message.listID = parseInt(listID);
+		let messages = restOfmessages.concat(message);
+		this.setState({ messages });
+	},
+
+	deleteMsg(listID, messageID) { 
+		let messages = this.state.messages.filter( message => message.messageID != messageID );
+		this.setState({ messages });
+	},
+
+	archiveMsg(listID, messageID) { 
+		let archivedMsg = this.state.messages.filter( message => message.messageID == messageID )[0];
+		archivedMsg.isArchived = !archivedMsg.isArchived;
+
+		let restOfmessages = this.state.messages.filter( message => message.messageID != messageID );
+		let messages = restOfmessages.concat(archivedMsg);
+
+		this.setState({ messages });
+	},
+	
+	render() {
+		let listProperties = this.state.lists.map( (list) => { 
+			return { listName: list.listName, listID: list.listID } 
+		});
+
+		let lists = this.state.lists.map( (list) => { 
+			let messages = this.state.messages.filter( message => message.listID === list.listID );
+
+			return <NamedList 
+				listID={list.listID} 
+				listName={list.listName} 
+				messages={messages} 
+				createMsg={this.createMsg} 
+				archiveMsg={this.archiveMsg} 
+				listProperties={listProperties}
+				moveMsg={this.moveMsg} 
+				deleteMsg={this.deleteMsg} 
+			/>; 
+		});
+
+		return (
+			<div>
+				<h3>Create New List</h3>
+				<input type="text" ref='inputField' onKeyDown={this.createList} placeholder="title" />
+				<button onClick={this.createList}>Create new list</button>
+				{lists}
+			</div>
+		);
+	}
+});
+
+ReactDOM.render( <Wrapper/>, document.getElementById('app'));
---
+++ pontus/week3/app/named-list.jsx
@@ -0,0 +1,91 @@
+let React = require('react');
+let Message = require('./message');
+
+let NamedList = React.createClass({ 
+	propTypes: { 
+		listID: React.PropTypes.number,
+		listName: React.PropTypes.string,
+		createMsg: React.PropTypes.func,
+		archiveMsg: React.PropTypes.func,
+		moveMsg: React.PropTypes.func,
+		deletesg: React.PropTypes.func,
+		messages: React.PropTypes.arrayOf(React.PropTypes.object),
+		listProperties: React.PropTypes.arrayOf(React.PropTypes.object)
+	},
+
+	getInitialState() {
+		return { 
+			charCount: 200,
+			isError: false,
+			errorMsg: "",
+			displayChars: false
+		};
+	},
+
+	createMsg(e) { 
+		if (e.keyCode == 13 || e.which == 13 || e.type == 'click') { 
+			let text = this.refs.inputField.value;
+			this.props.createMsg(this.props.listID, text);
+			this.refs.inputField.value = "";
+			this.refs.inputField.focus();
+		}
+	},
+
+	displayCharCount() { 
+		this.setState({ displayChars: !this.state.displayChars }); 
+	},
+
+	charValidation() { 
+		let charCount = 200 - this.refs.inputField.value.length;
+
+		if (charCount < 0) this.setState({ isError: true, errorMsg: "out of characters." });
+		else this.setState({ isError: false, errorMsg: "" }) 
+
+		this.setState({ charCount });
+	},
+
+ 	render() {
+		let archivedMessages = this.props.messages.filter( message => message.isArchived );
+
+		let allMessages = this.props.messages
+			.filter( message =>  !message.isArchived )
+			.concat(archivedMessages);
+
+		let messages = allMessages.map( (message) => {
+			return <Message 
+				listID={message.listID} 
+				messageID={message.messageID} 
+				text={message.text} 
+				isArchived={message.isArchived} 
+				listProperties={this.props.listProperties}
+				listName={this.props.listName}
+				archiveMsg={this.props.archiveMsg} 
+				moveMsg={this.props.moveMsg}
+				deleteMsg={this.props.deleteMsg}
+			/> ;
+		});
+
+		let errorMsg =  (this.state.isError) ? { color: 'red' } : { color: 'black'};
+		let btnState = (this.state.isError) ? true : false;
+		let displayChars = (this.state.displayChars) ? {display: 'inline'} : {display: 'none' };
+
+		return (
+			<div>
+				<h2>{this.props.listName}</h2>
+				<ul>
+					{messages}
+				</ul> 
+				<input onBlur={this.displayCharCount} onKeyDown={this.createMsg} onFocus={this.displayCharCount} onKeyUp={this.charValidation} ref='inputField' type="text" />
+				<button disabled={btnState} onClick={this.createMsg}>Create New Message</button>
+				<br/>
+				<div style={errorMsg}>
+					<span style={displayChars}> {this.state.charCount} </span>
+					<span>{this.state.errorMsg}</span>
+				</div>
+			</div>
+		);
+	}
+});
+
+
+module.exports = NamedList;
