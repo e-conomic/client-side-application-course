17c48e459da7b62262ba848ec04abea42c10085c
Peter Greve <pgr@e-conomic.com>
2016-03-01 16:17:48 +0100

    Done

---
+++ petergreve/week3/ass4/app/Components/ErrorMessage.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function () {
+
+        var style = {
+            color: 'red',
+        };
+        return  <div style={style}>
+                    {this.props.errorMessage}
+                </div>
+    }
+
+});
