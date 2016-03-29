4e68af5ca8d3ab852e45d030b7b181207974653c
Memphis <kakougr2000@yahoo.gr>
2016-03-29 16:02:16 +0200

    small changes

--- memphis88/week5/ass6/app/stores/validation-store.js
+++ memphis88/week5/ass6/app/stores/validation-store.js
@@ -2,13 +2,21 @@ var BaseStore = require('./base');
 var Constants = require('../dispatcher/constants');
 var Dispatcher = require('../dispatcher/dispatcher').Dispatcher;
 
+var _valid = false;
+
 store = Object.assign({}, BaseStore, {
-	getAll: function() {},
+	getStatus: function() {
+		return _valid;
+	},
 });
 
 store.dispatchToken = Dispatcher.register(function(payload) {
 	switch(payload.type) {
 		case Constants.EXCEED_RANGE_ERROR:
+			_valid = false;
+			break;
+		case Constants.CREATE_MESSAGE:
+			_valid = true;
 			break;
 		default:
 			return;
