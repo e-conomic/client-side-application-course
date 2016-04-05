efc23e96b13c2d23a841a9d6d306e3c5b6c39c49
dimitrisolaf <skj780@webmail.ku.dk>
2016-04-05 14:11:40 +0200

    tests now run with the new setup

--- dimitriosv/application/test/setup.js
+++ dimitriosv/application/test/setup.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-
+import polyfill from 'babel-polyfill';
 chai.use(sinonChai);
 
 global.expect = chai.expect;
