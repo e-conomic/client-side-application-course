6b8919ba4f4bced09cc23bdfae50b3b2f9f9606e
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-04-05 03:00:57 +0200

    A unit test

---
+++ denlillemand/application/app/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from './components/container.jsx';
+
+ReactDOM.render(<Container />, document.getElementById("root"));
---
+++ denlillemand/application/app/components/namedlistform.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ListStore from '../stores/liststore.js';
+import ListActions from '../actioncreators/listactions.js';
+
+export default class NamedListForm extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            name: ""
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(event) {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    onSubmit(event) {
+        ListActions.createList(this.state.name);
+        this.setState({
+            name: ""
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.name} onChange={this.onChange}/>
+                <button onClick={this.onSubmit}>Submit list</button>
+            </div>
+        );
+    }
+} 
