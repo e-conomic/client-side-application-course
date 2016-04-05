f0a110240d5ef42c4ccec592a9bd00918e46f973
dimitrisolaf <skj780@webmail.ku.dk>
2016-04-05 13:49:45 +0200

    continuing testing

--- dimitriosv/application/test/helloworld.js
+++ dimitriosv/application/test/helloworld.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 import ReactTestUtils from 'react-addons-test-utils'
 import React from 'react'
-import Message from '../app/components/hello-world'
-
+import Hello from '../app/components/hello-world'
+import Message from '../app/components/Message'
+import ListStore from '../app/stores/list-store'
 
 
 describe('hello world', () => {
@@ -11,13 +12,28 @@ describe('hello world', () => {
   });
 });
 
-describe('Message component', function() {
+describe('Hello component', function() {
     const shallowRenderer = ReactTestUtils.createRenderer();
     describe('when rendered', function() {
         it('should return the expected output', function() {
-            shallowRenderer.render(<Message name='World' />);
+            shallowRenderer.render(<Hello name='World' />);
             const output = shallowRenderer.getRenderOutput();
             expect(output.props.children).to.equal('Hello World');
+        });
+    });
+});
+
+
+
+describe('Message component', function() {
+    const shallowRenderer2 = ReactTestUtils.createRenderer();
+    var lists = ListStore.getAll();
+    //var lists=[{listId: 0, listName: "first list1"},{listId: 1, listName: "second list2"}];
+    describe('when rendered', function() {
+        it('should return the expected output', function() {
+            shallowRenderer2.render(<Message messageId='1'  messageText='hello' messageIsArchived='false' allLists={lists} />);
+            const output2 = shallowRenderer2.getRenderOutput();
+            expect(true).to.be.true;
         });
     });
 });
