0acf575752fd2e0fc9b42c41bbcbe751245dea1a
Peter Greve <peter.grv@gmail.com>
2016-04-12 09:37:45 +0200

    removed dependency to message-action form test'

--- petergreve/application/test/stores/message-store.js
+++ petergreve/application/test/stores/message-store.js
@@ -1,35 +1,32 @@
 var rewire = require("rewire");
-
+var Constants = require('../../app/constants/constants')
 var MessageStore = rewire('../../app/stores/message-store')
-import MessageActions from '../../app/actions/message-actions'
-import dispatcher from '../../app/dispatcher/dispatcher'
+import Dispatcher from '../../app/dispatcher/dispatcher'
 
 describe('Message Store methods', () => {
 	
-	let spy;
 	before(() => {
-        spy = sinon.spy(dispatcher, "dispatch");
         MessageStore.__set__('_messages',[])
 	});
 
-	after(() => {
-        dispatcher.dispatch.restore();
-	});
-
-
 	describe('Create Message', () => {
-		let  message = {listId: 0, text: 'test message'}
 
-
-
-		it('should create a message', () => {
-			MessageActions.createMessage(message);
-
-			expect(spy).to.have.been.calledOnce;
+		it('should create a message with correct properties', () => {
+			Dispatcher.dispatch({
+                type: Constants.CREATE_MESSAGE,
+                listId: 0,
+                text: 'test message'
+            });
 
 			const messages = MessageStore.getAll()
+			let message = messages[0];
 
 			expect(messages.length).to.equal(1);
+			expect(message.id).to.equal(0);
+			expect(message.listId).to.equal(0);
+			expect(message.text).to.equal('test message');
+			expect(message.isArchived).to.equal(false);
+			expect(message.translation).to.equal('');
 
 		})
 
