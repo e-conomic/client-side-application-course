87a4e97ce786bb63f0e68bd3e84422cbe4c28366
Dionysios Kakouris <kakougr2000@yahoo.gr>
2016-04-19 10:38:01 +0200

    added istanbul config file and minor improvements to message store test. added react test utils as global in tests

--- memphis88/application/test/setup.js
+++ memphis88/application/test/setup.js
@@ -3,6 +3,8 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import rewire from 'rewire';
 import Constants from '../app/dispatcher/constants';
+import ReactTestUtils from 'react-addons-test-utils';
+
 
 chai.should();
 chai.use(sinonChai);
@@ -11,3 +13,4 @@ global.expect = chai.expect;
 global.sinon = sinon;
 global.rewire = rewire;
 global.Constants = Constants;
+global.ReactTestUtils = ReactTestUtils;
--- memphis88/application/test/stores/message-store.js
+++ memphis88/application/test/stores/message-store.js
@@ -34,37 +34,49 @@ describe('Message Store', function() {
 		registerStub.should.have.been.calledOnce;
 	});
 
-	it('should return an empty object if an id does not exist or is not specified', function() {
-		var message = MessageStore.get();
-		expect(message).to.be.a('object');
-		expect(message).to.be.empty;
-		message = MessageStore.get('an invalid id');
-		expect(message).to.be.a('object');
-		expect(message).to.be.empty;
+	describe('get', function() {
+
+		it('should return an empty object if an id does not exist or is not specified', function() {
+			var message = MessageStore.get();
+			expect(message).to.be.a('object');
+			expect(message).to.be.empty;
+			message = MessageStore.get('an invalid id');
+			expect(message).to.be.a('object');
+			expect(message).to.be.empty;
+		});
 	});
 
-	it('should create and push a message from the dispatched action', function() {
-		Dispatcher.dispatch(createMessage);
-		var message = MessageStore.getAll();
-		expect(message[0]).to.be.a('object');
-		expect(message[0].message).to.be.equal('A test message');
+	describe('getAll', function() {
+
+		it('should create and push a message from the dispatched action', function() {
+			Dispatcher.dispatch(createMessage);
+			var message = MessageStore.getAll();
+			expect(message[0]).to.be.a('object');
+			expect(message[0].message).to.be.equal('A test message');
+		});
 	});
 
-	it('should return all messages from a specified list', function() {
-		Dispatcher.dispatch(createMessage);
-		var message = MessageStore.getAllForList(0);
-		expect(message).to.be.a('array');
-		expect(message.length).to.be.equal(1);
+	describe('getAllForList', function() {
+
+		it('should return all messages from a specified list', function() {
+			Dispatcher.dispatch(createMessage);
+			var message = MessageStore.getAllForList(0);
+			expect(message).to.be.a('array');
+			expect(message.length).to.be.equal(1);
+		});
 	});
 
-	it('should return a list of alphabetically-ordered messages', function() {
-		Dispatcher.dispatch(createAnotherMessage);
-		Dispatcher.dispatch(createMessage);
-		var allMessages = MessageStore.getAllSorted();
-		expect(allMessages.length).to.be.equal(2);
-		expect(allMessages[0]).to.be.a('object');
-		expect(allMessages[0].message).to.be.equal('A test message');
-		expect(allMessages[1]).to.be.a('object');
-		expect(allMessages[1].message).to.be.equal('This message should appear second');
+	describe('getAllSorted', function() {
+
+		it('should return a list of alphabetically-ordered messages', function() {
+			Dispatcher.dispatch(createAnotherMessage);
+			Dispatcher.dispatch(createMessage);
+			var allMessages = MessageStore.getAllSorted();
+			expect(allMessages.length).to.be.equal(2);
+			expect(allMessages[0]).to.be.a('object');
+			expect(allMessages[0].message).to.be.equal('A test message');
+			expect(allMessages[1]).to.be.a('object');
+			expect(allMessages[1].message).to.be.equal('This message should appear second');
+		});
 	});
 });
